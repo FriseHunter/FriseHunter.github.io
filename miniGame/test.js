095ce85f8950167074f9838231f9dd978f17f0c9
FriseHunter <freesoul2708@gmail.com>
2017-06-07 16:33:01 +0300

    miniGame

---
+++ miniGame/test.js
@@ -0,0 +1,17 @@
+// bidimensional array
+// 2 x 2
+// var grid =  [
+// // 0,1
+//   [1,2],//0
+//   [3,4] //1
+// ];
+//
+// grid[ row ][ col ];
+// function showScore() {
+//       var addScore = document.getElementById("score");
+//       var score = 25;
+//       score += 5;
+//       addScore.innerHTML = score;
+//       // document.getElementById("score") = score += 5;
+// }
+// showScore();
