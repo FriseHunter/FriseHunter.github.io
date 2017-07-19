58d9af983d10ef103ca916dfcd3a7043fb11d029
FriseHunter <freesoul2708@gmail.com>
2017-07-19 21:08:40 +0300

    Add files via upload

--- to-do-express/index.js
+++ to-do-express/index.js
@@ -1,9 +1,13 @@
 const express = require('express')//library
 const app = express(); //application
-const swig = require('swig')
+const swig = require('swig');
+const bodyParser = require('body-parser');//process post/get data
+const expressValidator = require('express-validator');//process post/get data
 
+//MIDDLEWEARE
 app.use(express.static('public'));//middleWare - promejutochnaz obrabotka
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 //CONFIG
 app.engine('html', swig.renderFile);
@@ -19,8 +23,41 @@ app.get('/register', (req,res)=>{
 res.render('register-page')});
 app.get('/login', (req,res)=>{
 res.render('login-page')});
-app.get('/task', (req,res)=>{
-res.render('task-page')});
+app.post('/user/create', (req,res)=>{
+  var fullname  = req.body.fullname
+  var email     = req.body.email
+  var password  = req.body.password
+  var cpassword = req.body.cpassword
+//1)sanitization
+
+//2)validation
+if (fullname.length < 3) {
+  //error
+  res.render('register-page',{error:'Fullname must be at least 3 simbols'});
+}
+//3)save tro datebase
+
+else if (email.length < 3) {
+  //error
+res.render('register-page',{error:'E - mail must be at least 3 simbols'});
+}
+//3)save tro datebase
+
+else if (password.length < 3) {
+  //error
+  res.render('register-page',{error:'Password must be at least 3 simbols'})
+}
+//3)save tro datebase
+
+else if (cpassword.length < 3) {
+  //error
+  res.render('register-page',{error:'Confirm Password must be at least 3 simbols'})
+}
+//3)save tro datebase
+else {
+  res.send('All good');
+}
+});
 
 
 
@@ -34,3 +71,5 @@ app.get('/', (req, res)=>{
 app.listen(3000, () => {
   console.log('Express server app running!!!');
 });
+
+// /user/login <<< post();
