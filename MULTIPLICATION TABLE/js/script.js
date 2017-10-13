95b45e9b08f791405e076c5e9cfe457708d4949a
Serghey <freesoul2708@gmail.com>
2017-10-13 15:22:21 +0300

    MULTIPLICATION TABLE

---
+++ MULTIPLICATION TABLE/js/script.js
@@ -0,0 +1,18 @@
+var table = '';
+var y = "";
+var el = document.getElementById("text");
+var elTbl = document.getElementById("table");
+
+function getNumber(){
+
+	var text = el.value;
+	y = text;
+		for(var i = 1; i<=10; i++){
+				table += `${y} x ${i} = ${(y*i)} </br>`
+		}
+	elTbl.innerHTML = table;
+}
+
+function clear__text(){
+     window.location.reload();
+}
