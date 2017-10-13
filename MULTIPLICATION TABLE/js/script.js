87cce7081e8002ca01d492821d1a0d81c241dbed
Serghey <freesoul2708@gmail.com>
2017-10-13 16:39:12 +0300

    overwrite form

--- MULTIPLICATION TABLE/js/script.js
+++ MULTIPLICATION TABLE/js/script.js
@@ -3,16 +3,21 @@ var y = "";
 var el = document.getElementById("text");
 var elTbl = document.getElementById("table");
 
-function getNumber(){
+function getNumber() {
 
-	var text = el.value;
-	y = text;
-		for(var i = 1; i<=10; i++){
-				table += `${y} x ${i} = ${(y*i)} </br>`
-		}
-	elTbl.innerHTML = table;
+      var text = el.value;
+      y = text;
+
+      for (var i = 1; i <= 10; i++) {
+            table += `${y} x ${i} = ${(y*i)} </br>`;
+
+      }
+      elTbl.innerHTML = table;
+      table = "";
+      console.log(text);
+
 }
 
-function clear__text(){
-     window.location.reload();
+function clear__text() {
+      window.location.reload();
 }
