922d48456cfc22a7afcf7a5175d693d942655342
Serghey <freesoul2708@gmail.com>
2017-12-26 16:11:01 +0200

    icons_creack

--- Landing_Занятный_Дом/js/script.js
+++ Landing_Занятный_Дом/js/script.js
@@ -5,3 +5,24 @@ if (top < 70) $("#fixmenu").css({top: '0', position: 'relative', });
 else $("#fixmenu").css({top: '0', position: 'fixed', width: '100%', transition: '0.3s',});
 });
 });
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
