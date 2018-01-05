55113a76013351a6f5b03e41e12f84fa4ca9c6b0
Serghey <freesoul2708@gmail.com>
2018-01-05 15:10:00 +0200

    add js

--- Landing_Занятный_Дом/js/script.js
+++ Landing_Занятный_Дом/js/script.js
@@ -1,28 +1,15 @@
-$(function(){
-$(window).scroll(function() {
-var top = $(document).scrollTop();
-if (top < 70) $("#fixmenu").css({top: '0', position: 'relative', });
-else $("#fixmenu").css({top: '0', position: 'fixed', width: '100%', transition: '0.3s',});
-});
-});
-
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "../mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
+$(function() {
+      $(window).scroll(function() {
+            var top = $(document).scrollTop();
+            if (top < 70) $("#fixmenu").css({
+                  top: '0',
+                  position: 'relative',
+            });
+            else $("#fixmenu").css({
+                  top: '0',
+                  position: 'fixed',
+                  width: '100%',
+                  transition: '0.3s',
+            });
+      });
 });
