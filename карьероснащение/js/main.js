a3f7e7dc6a06ab3baa91f5f699dc358289fd1943
Serghey <freesoul2708@gmail.com>
2018-02-26 16:08:38 +0200

    add news archiv

--- карьероснащение/js/main.js
+++ карьероснащение/js/main.js
@@ -4,14 +4,27 @@ $(function() {
             if (top < 100) $("#fixmenu").css({
                   top: '0',
                   position: 'relative',
-                  zIndex: '50' ,
+                  zIndex: '50',
             });
             else $("#fixmenu").css({
                   top: '0',
                   position: 'fixed',
                   width: '100%',
-                  zIndex: '50' ,
+                  zIndex: '50',
                   transition: '0.3s',
             });
       });
 });
+
+function dBlock(e) {
+      e = e || window.event;
+      var src = e.target || e.srcElement;
+      el = src.parentNode.parentNode.getElementsByClassName('txtMainNews');
+      if (el[0].style.display == "block") {
+            console.log(el[0].style.display);
+            el[0].style.display = 'none';
+            console.log(el[0].style.display);
+      } else {
+            el[0].style.display = 'block';
+      }
+}
